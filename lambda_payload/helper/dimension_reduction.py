26c406dc90a33f707f10c3e807a2bf408c4af6cf
Joshua Head <joshuascomputer@Joshuas-MBP.lan>
2024-05-01 23:18:27 -0400

    test alerts

--- lambda_payload/helper/dimension_reduction.py
+++ lambda_payload/helper/dimension_reduction.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+import time
 
 class PCA():
     def __init__(self, components:int, X):
@@ -29,57 +31,15 @@ class PCA():
 
         return explained_variances, cumulative_variance
 
+
 class AutoEncoder(nn.Module):
-
-    def __init__(self, latent_components:int, transport_layer_count:int, X):
-        self.latent_components = latent_components
-        self.transport_layer_count = transport_layer_count
-        self.features = torch.from_numpy(X).float()
-
-    def __len__(self):
-        return len(self.features)
-    
-    def transition_layers(self):
-
-        jumps = int((self.__len__() - self.latent_components) / self.transport_layer_count)
-
-        new_layers = []
-        current_size = self.__len__()
-
-        for i in range(1, self.transport_layer_count + 1):
-            next_size = current_size - jumps
-            new_layers.append(nn.Linear(current_size, next_size))
-            new_layers.append(nn.ReLU())
-            current_size = next_size
-    def get_covariance(self):
-        return np.cov(self.standarize_data, rowvar=False)
-    
-    def get_eigenvalues(self):
-        eigenvalues, eigenvectors = np.linalg.eigh(self.get_covariance)
-        return eigenvalues[::-1], eigenvectors[:, ::-1]
-
-    def get_pca(self, X):
-
-        eigenvalues, eigenvectors = self.get_eigenvalues(
-            self.get_covariance())
-
-        pc = eigenvectors[:, :self.components]
-        return self.standarize_data @ pc
-    
-    def explained_variance(eigenvalues):
-        total_variance = np.sum(eigenvalues)
-        explained_variances = eigenvalues / total_variance
-        cumulative_variance = np.cumsum(explained_variances)
-
-        return explained_variances, cumulative_variance
-
-class AutoEncoder():
 
     def __init__(self, latent_components:int, transport_layer_count:int, X):
         super(AutoEncoder, self).__init__()
         self.latent_components = latent_components
         self.transport_layer_count = transport_layer_count
         self.features = torch.from_numpy(X).float()
+
         self.encoder = nn.Sequential(
             nn.Linear(self.__len__(), 8),
             nn.ReLU(),
@@ -94,7 +54,7 @@ class AutoEncoder():
         )
 
     def __len__(self):
-        return len(self.features)
+        return self.features.shape[1]
     
     # not implimented will be at some point
     def transition_layers(self):
@@ -117,13 +77,16 @@ class AutoEncoder():
         decoded = self.decoder(encoded)
         return decoded
 
-    def train(self, epochs, learning_rate):
+    def train_model(self, epochs, learning_rate):
         self.train()
+        
         optimizer = optim.Adam(self.parameters(), lr=learning_rate)
         criterion = nn.MSELoss()
 
         for epoch in range(epochs):
+            start_time = time.time()
             total_loss = 0
+            print(f"start... epoch: {epoch}")
             for inputs in self.features:
                 optimizer.zero_grad()
                 outputs = self.forward(inputs)
@@ -131,6 +94,7 @@ class AutoEncoder():
                 loss.backward()
                 optimizer.step()
                 total_loss += loss.item()
+            print(f"end... epoch: {epoch} --- loss: {total_loss} --- Total time: {time.time() - start_time:.2f}")
     
     def get_encoder(self, data):
         self.eval()
